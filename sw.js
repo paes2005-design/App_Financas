da0312ae27e71fc3c3e8c5d51a1127eac133cc65
paes2005-design <paes2005@gmail.com>
2026-08-07 05:46:24 -0300

    Refresh PWA cache after stability fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app-financas-static-v36";
+const CACHE_NAME = "app-financas-static-v37";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -29,6 +29,7 @@ const APP_SHELL = [
   "./js/transferencias.js",
   "./js/cartoes.js",
   "./js/transaction-actions.js",
+  "./js/quick-settlement.js",
   "./js/recurrence.js",
   "./js/recurrence-period-bridge.js",
   "./js/planned-settlement.js",
